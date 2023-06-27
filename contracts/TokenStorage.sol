23f05f7a0a7faa4a7cc4484d41864d0910efdeaf
abrosua <abrosua@gmail.com>
2023-06-27 21:10:32 +0700

    handle receive token with permit on the TokenStorage

--- contracts/TokenStorage.sol
+++ contracts/TokenStorage.sol
@@ -3,7 +3,8 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./IERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20BurnPermit} from "./IERC20BurnPermit.sol";
 
 error ZeroBalance();
 
@@ -11,18 +12,22 @@ error ZeroBalance();
 This storage should be able received ERC-20 token and record the current balance
 */
 contract TokenStorage is Ownable {
+    using SafeERC20 for IERC20;
+
     // event
     event TokensSent(address to, address token, uint256 amount);
     event TokensReceived(address from, address token, uint256 amount);
     event TokensWithdrew(address token, uint256 amount);
     event TokensBurnt(address token, uint256 amount);
 
-    IERC20Burnable private _token;
+    IERC20BurnPermit private _token;
+    IERC20 private _tokenSafe;
     string public tokenName;
     string public tokenSymbol;
 
     constructor(address token) {
-        _token = IERC20Burnable(token);
+        _token = IERC20BurnPermit(token);
+        _tokenSafe = IERC20(token);
         tokenName = _token.name();
         tokenSymbol = _token.symbol();
     }
@@ -51,6 +56,34 @@ contract TokenStorage is Ownable {
         require(allowance >= amount, "Insufficient allowance! Check it again");
         // perform the token transfer from sender to storage
         _token.transferFrom(msg.sender, address(this), amount);
+        emit TokensReceived(msg.sender, address(_token), amount);
+    }
+
+    /*
+     * To send tokens from the caller to the storage with permit signature!
+     */
+    function receiveTokenPermit(
+        uint256 amount,
+        uint256 deadline,
+        uint8 permitV,
+        bytes32 permitR,
+        bytes32 permitS
+    ) external {
+        require(amount > 0, "Amount must be greater than zero");
+        // handle ERC20 permit signature
+        {
+            _token.permit(
+                msg.sender,
+                address(this),
+                amount,
+                deadline,
+                permitV,
+                permitR,
+                permitS
+            );
+        }
+        // perform the token transfer from sender to storage
+        _tokenSafe.safeTransferFrom(msg.sender, address(this), amount);
         emit TokensReceived(msg.sender, address(_token), amount);
     }
 
